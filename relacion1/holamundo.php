bdc86f806e9818dcae166fcf61373d927dcd6844
OravlaGG <166486822+OravlaGG@users.noreply.github.com>
2025-09-16 19:56:11 +0200

    primera subida drag and drop

---
+++ relacion1/holamundo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relacion 1 - Hola Mundo</title> 
+    <link rel="shortcut icon" href="playamar.png" type="image/x-icon">
+</head>
+<body>
+    
+    <h1>Hola Mundo en php</h1>
+    <?php 
+        echo "<h3 style='color:red'>Hola Mundo</h3>"
+    ?>
+
+</body>
+</html>
